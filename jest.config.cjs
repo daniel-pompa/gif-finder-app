eb2e88f20a725423d8a6f2461c3f2f6188e17aec
Daniel <daniel.40.pompa@gmail.com>
2024-08-27 18:57:11 +0200

    chore: enhance Jest setup
    - Added `babel-jest` for transforming JS and JSX files.
    - Configured `identity-obj-proxy` to handle CSS imports.
    - Excluded `node_modules` from transformations.

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,11 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['./jest.setup.js'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest', // Transform JS and JSX files with babel-jest
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Use identity-obj-proxy to handle CSS imports
+  },
+  transformIgnorePatterns: ['/node_modules/'],
 };
